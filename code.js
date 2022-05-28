f73b56a820d1333045dc54477d96c192dec43bea
Luisjauregui6 <luisjauregui221@gmail.com>
2022-05-27 20:28:42 -0500

    update

--- code.js
+++ code.js
@@ -7,17 +7,44 @@ function openNav() {
   }
 
 //this function will add a clock to the main page  
+// function tellTime() {
+//   let now = new Date();
+//   let h = now.getHours();
+//   let m = now.getMinutes();
+//   let s = now.getSeconds();
+//   m = checkTime(m);
+//   s = checkTime(s);
+//   document.getElementById("clockContainer").innerHTML = h + ":" + m + ":" + s;
+//   let t = setTimeout(tellTime, 500);
+// }
+//  function checkTime(i) {
+//     if (i < 10) {i = "0" + i};
+//     return i;
+// }
+
+//function to add a clock to the main page
 function tellTime() {
   let now = new Date();
-  let h = now.getHours();
-  let m = now.getMinutes();
-  let s = now.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById("clockContainer").innerHTML = h + ":" + m + ":" + s;
-  let t = setTimeout(tellTime, 500);
+  let hh = now.getHours();
+  let mm = now.getMinutes();
+  let ss = now.getSeconds();
+  let session = "AM";
+
+  if (hh == 0) {
+hh = 12;
+  }
+  if (hh > 12) {
+hh = hh  -12;
+session = "PM"
+  }
+
+  hh = (hh < 10) ? "0" + hh : hh;
+  mm = (mm < 10) ? "0" + mm : mm;
+  ss = (ss < 10) ? "0" + ss : ss;
+  
+let time = hh + ":" + mm + ":" + ss + " " + session;  
+document.getElementById("clockDiv").innerText = time;
+
+let t = setTimeout(function(){ tellTime() } ,1000)
 }
- function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
-}
+tellTime();
